aa53443c2921f1a5db0921d5869f434e2490bdf9
Sergo7777 <serega_denisenko@mail.ru>
2017-02-24 20:05:54 +0200

    commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question
 from .models import Choice
+from .models import UserAnswer
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(UserAnswer)
